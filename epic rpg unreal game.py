06fe74da0f3b9593c51125bb0f7202c4b42d25a5
NikGapon <45200250+NikGapon@users.noreply.github.com>
2019-02-21 21:26:03 +0400

    v3.2.5

--- epic rpg unreal game.py
+++ epic rpg unreal game.py
@@ -496,7 +496,6 @@ Player_Hero = Hero(player_class)
 XP_boots_25_1 = inv_eqip_upd('Xp_boost_+25_v2.png', ['XP', 25])
 XP_boots_10_1 = inv_eqip_upd('Xp_boost_+10_v2.png', ['XP', 25])
 MP_boots_20_1 = inv_eqip_upd('Mp_boost_+20_v2.png', ['MP', 20])
-
 # --------------START--------------
 Player_Hero.apend_inv_hero(XP_boots_25_1)
 Player_Hero.apend_inv_hero(XP_boots_10_1)
